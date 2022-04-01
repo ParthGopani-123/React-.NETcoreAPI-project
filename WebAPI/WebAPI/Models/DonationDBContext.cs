eeac24f4c599ef1706b1afe1fc21d587f7d63326
ParthGopani-123 <Parth@3461>
2022-04-01 14:08:04 -0400

    CRUD Operation Draft Done

---
+++ WebAPI/WebAPI/Models/DonationDBContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class DonationDBContext : DbContext
+    {
+
+        // Parameterless Constructor
+        public DonationDBContext()
+        {
+
+        }
+
+        public DonationDBContext(DbContextOptions<DonationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DCandidate> DCandidates { get; set; }
+    }
+}
